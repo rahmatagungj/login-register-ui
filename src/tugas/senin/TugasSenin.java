44f7304cd43a9040245c4a0f8022f471f2234022
rahmatagungj <rahmatagungj@gmail.com>
2021-05-29 17:09:21 +0700

    new deploy

---
+++ src/tugas/senin/TugasSenin.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.senin;
+
+/**
+ *
+ * @author rahmatagungj
+ */
+public class TugasSenin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Register rg = new Register();
+        rg.setVisible(true);
+        rg.pack();
+        rg.setLocationRelativeTo(null);
+        rg.setDefaultCloseOperation(Register.EXIT_ON_CLOSE);
+        
+    }
+    
+}
